716f0d9c84d7beefdb8126fa5d6bf7ba936ceb72
guidowb <guidowb@gmail.com>
2014-10-15 18:33:11 -0500

    Added a custom login page. Leveraging Spring security for CSRF protection required server-side page templates - switched from our own lightweight web server to Thymeleaf for that purpose.

--- Ancestory/src/main/java/org/guidowb/ancestory/main/SecurityConfig.java
+++ Ancestory/src/main/java/org/guidowb/ancestory/main/SecurityConfig.java
@@ -3,11 +3,14 @@ package org.guidowb.ancestory.main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -15,5 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	http
+    		.authorizeRequests()
+    			.antMatchers("/css/**").permitAll()
+    			.antMatchers("/images/**").permitAll()
+    			.anyRequest().authenticated()
+    			.and()
+    		.formLogin()
+    			.loginPage("/login")
+    			.permitAll()
+    			.and()
+    		.logout()
+    			.permitAll();
     }
 }
