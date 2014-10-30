23ec13ee038ca7655511e5565e8c9e2c6c344770
guidowb <guidowb@gmail.com>
2014-10-30 13:14:24 -0500

    Using AngularJS 1.3 validators for user form validation

--- Ancestory/src/main/java/org/guidowb/ancestory/api/UserService.java
+++ Ancestory/src/main/java/org/guidowb/ancestory/api/UserService.java
@@ -3,10 +3,12 @@ package org.guidowb.ancestory.api;
 import org.guidowb.ancestory.security.User;
 import org.guidowb.ancestory.security.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +25,7 @@ public class UserService {
 	@RequestMapping(value="/api/users/{username}", method=RequestMethod.GET)
 	public User getUser(@PathVariable String username) {
 		User user = users.findByUsername(username);
+		if (user == null) throw new UserNotFoundException();
 		return user;
 	}
 
@@ -35,7 +38,7 @@ public class UserService {
 	@RequestMapping(value="/api/users/{username}", method=RequestMethod.PUT)
 	public void updateUser(@PathVariable String username, @RequestBody User update) {
 		User user = users.findByUsername(username);
-		if (user == null) ; // TODO - throw not found
+		if (user == null) throw new UserNotFoundException();
 		user.update(update);
 		validateUser(user);
 		users.save(user);
@@ -44,19 +47,24 @@ public class UserService {
 	@RequestMapping(value="/api/users/{username}", method=RequestMethod.DELETE)
 	public void deleteUser(@PathVariable String username) {
 		User user = users.findByUsername(username);
-		if (user == null) ; // TODO - throw not found
+		if (user == null) throw new UserNotFoundException();
 		users.delete(user);
 	}
 
 	private void validateUser(User user) {
-		if (user.getUsername() == null) {
-			// TODO - Throw appropriate exception
-		}
-		if (user.getUsername().isEmpty()) {
-			// TODO - Throw appropriate exception
-		}
-		if (!user.hasPassword()) {
-			// TODO - Throw appropriate exception
-		}
+		if (user.getUsername() == null || user.getUsername().isEmpty()) throw new UsernameRequiredException();
+		if (!user.hasPassword()) throw new PasswordRequiredException();
 	}
+	
+	@SuppressWarnings("serial")
+	@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such user")
+	private class UserNotFoundException extends RuntimeException {}
+
+	@SuppressWarnings("serial")
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Username is required")
+	private class UsernameRequiredException extends RuntimeException {}
+
+	@SuppressWarnings("serial")
+	@ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="Password is required")
+	private class PasswordRequiredException extends RuntimeException {}
 }
--- Ancestory/src/main/resources/static/js/userdialogController.js
+++ Ancestory/src/main/resources/static/js/userdialogController.js
@@ -1,9 +1,10 @@
 ancestoryControllers.controller('userdialogController', ['$scope', '$modalInstance', '$http',
 	function ($scope, $dialog, $http) {
-		$scope.username = {};
-		$scope.password = {};
+		$scope.username = '';
+		$scope.password = '';
+		$scope.password2 = '';
 		$scope.ok = function() {
-			var user = { username: $scope.username.value, password: $scope.password.value };
+			var user = { username: $scope.username, password: $scope.password };
 			$http.post('/api/users', user).success(function(data) {
 				$dialog.close($scope.user);
 			});
@@ -11,23 +12,60 @@ ancestoryControllers.controller('userdialogController', ['$scope', '$modalInstan
 		$scope.cancel = function() {
 			$dialog.dismiss('cancel');
 		}
-		$scope.valid = function() {
-			if (!$scope.username.valid()) return false;
-			if (!$scope.password.valid()) return false;
-			return true;
-		}
-		$scope.username.valid = function() {
-			$scope.username.errors = {};
-			if ($scope.username.value == null || $scope.username.value == '') $scope.username.errors.unset = true;
-			for (key in $scope.username.errors) return false;
-			return true;
-		}
-		$scope.password.valid = function() {
-			$scope.password.errors = {};
-			if ($scope.password.value == null || $scope.password.value == '') $scope.password.errors.unset = true;
-			if ($scope.password.value != $scope.password.repeat) $scope.password.errors.dontmatch = true;
-			for (key in $scope.password.errors) return false;
-			return true;
-		}
 	}
 ]);
+
+ancestoryControllers.directive('usernameValidator', [ '$q', '$http', function($q, $http) {
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		link: function(scope, element, attrs, ngModel) {
+			if (!ngModel) return;
+			if (!ngModel.$validators) return;
+			console.log(ngModel);
+			ngModel.$validators.required = function(modelValue, viewValue) {
+				if (ngModel.$isEmpty(modelValue)) return false;
+				return true;
+			}
+			ngModel.$asyncValidators.unique = function(modelValue, viewValue) {
+				var deferred = $q.defer();
+				$http.get('/api/users/' + modelValue).success(function() {
+					deferred.reject(); // Username was found
+				}).error(function() {
+					deferred.resolve(); // Either not found or we can't be sure
+				});
+				return deferred.promise;
+			}
+		}
+	}
+}]);
+
+ancestoryControllers.directive('passwordValidator', function() {
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		link: function(scope, element, attrs, ngModel) {
+			if (!ngModel) return;
+			if (!ngModel.$validators) return;
+			ngModel.$validators.required = function(modelValue, viewValue) {
+				if (ngModel.$isEmpty(modelValue)) return false;
+				return true;
+			}
+		}
+	}
+});
+
+ancestoryControllers.directive('equals', function() {
+	return {
+		restrict: 'A',
+		require: '?ngModel',
+		link: function(scope, element, attrs, ngModel) {
+			if (!ngModel) return;
+			if (!ngModel.$validators) return;
+			attrs.$observe('equals', function() { ngModel.$validate(); })
+			ngModel.$validators.equals = function(value) {
+				return value == attrs.equals;
+			}
+		}
+	}
+});
